869bbabadfa7f4981014cc3fa19b30d023128a3a
SalchukArtem <archik.sv@gmail.com>
2024-01-10 21:01:19 +0200

    Task 2.3: Students count in the cities.

--- lab2.sql
+++ lab2.sql
@@ -1,3 +1,5 @@
 -- Task 2.2: Maximal average grade
-SELECT MAX(average_grade_per_year) AS max_average_grade
-FROM Student_Grades;
+SELECT city, COUNT(*) AS student_count
+FROM Student_Grades
+GROUP BY city
+ORDER BY student_count
