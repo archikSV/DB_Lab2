e7e68cc8cf1288315b63c119e5920cb1f67eb13d
SalchukArtem <archik.sv@gmail.com>
2024-01-10 19:33:01 +0200

    Select *

--- lab1.sql
+++ lab1.sql
@@ -1,15 +1,2 @@
---Practice 1: Create table.
-CREATE TABLE Students(
-student_id SERIAL PRIMARY KEY,
-student_name VARCHAR(50),
-age INT,
-email VARCHAR(50)
-);
-
-INSERT INTO Students (student_name, age, email) 
-VALUES 
-    ('John Doe', 20, 'john@example.com'),
-    ('Alice Smith', 22, 'alice@example.com'),
-    ('Bob Johnson', 21, 'bob@example.com'),
-    ('Emily Davis', 19, 'emily@example.com'),
-    ('Michael Wilson', 23, 'michael@example.com');
+--Practice 1: Select.
+SELECT * FROM Students;
