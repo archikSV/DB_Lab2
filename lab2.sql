5cbd1672bdb118239555372a1477508809632ace
SalchukArtem <archik.sv@gmail.com>
2024-01-10 21:02:48 +0200

    Task 2.4: Student count in the countries.

--- lab2.sql
+++ lab2.sql
@@ -1,5 +1,5 @@
--- Task 2.2: Maximal average grade
-SELECT city, COUNT(*) AS student_count
+-- Task 2.4: Student count in the countries
+SELECT country, COUNT(*) AS student_count
 FROM Student_Grades
-GROUP BY city
-ORDER BY student_count
+GROUP BY country
+ORDER BY student_count;
