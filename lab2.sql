5617dc5daf1747f537adb8d4b268575b2e333900
SalchukArtem <archik.sv@gmail.com>
2024-01-10 21:11:37 +0200

    Task 2.8: Average group grade

--- lab2.sql
+++ lab2.sql
@@ -1,4 +1,4 @@
--- Task 2.7: Student count in groups
-SELECT group_name, COUNT(*) AS student_count
+-- Task 2.8: Average group grade
+SELECT group_name, AVG(average_grade_per_year) AS avg_group_grade
 FROM Student_Grades
 GROUP BY group_name;
