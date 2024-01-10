1ceca1b1c957bfb3573bff5b0f3eb2f60c2961d0
SalchukArtem <archik.sv@gmail.com>
2024-01-10 19:59:22 +0200

    Added data to Courses.

--- lab1.sql
+++ lab1.sql
@@ -1,6 +1,9 @@
---Practice 1: Create Courses table with assosiation to Students table.
-CREATE TABLE Courses (
-course_id SERIAL PRIMARY KEY,
-course_name VARCHAR(100),
-student_id INTEGER REFERENCES Students(student_id)
-)
+--Practice 1: Add data to Courses.
+INSERT INTO Courses (course_name, student_id)
+VALUES ('Mathematics', 1),
+       ('Physics', 2),
+	   ('Information Technologies', 2),
+       ('Biology', 4),
+	   ('Art', 4),
+	   ('Biology', 1);
+
